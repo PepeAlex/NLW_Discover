269bb49686cd2319f24905e936bc660e12ec83dd
PepeAlex <G00376284@gmit.ie>
2021-06-29 13:10:47 +0100

    5th day

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,42 +1,43 @@
-import Modal from "./modal.js"
+import Modal from './modal.js'
 
 const modal = Modal()
 
-const modalTitle = document.querySelector(".modal h2")
-const modalDescription = document.querySelector(".modal p")
-const modalButton = document.querySelector(".modal button")
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
 
-const checkButtons = document.querySelectorAll(".actions a.check")
 
-checkButtons.forEach( button => {
+//Pegar todos os botões que existe com a classe check
+const checkButtons = document.querySelectorAll(".actions a.check") 
+
+checkButtons.forEach(button => {
+    //adicionar a escuta
     button.addEventListener("click", handleClick)
 })
 
+
+/*Quando o botão delete for clicado ele abre a modal */
 const deleteButton = document.querySelectorAll(".actions a.delete")
 
 deleteButton.forEach(button => {
-    button.addEventListener("click", (evente) => handleClick(event, false))
+    button.addEventListener("click", (event) => handleClick(event, false))
 })
 
-function handleClick(event, check = true) {
+function handleClick(event, check = true){
     event.preventDefault()
-
+    const text = check ? "Marcar como lida" : "Excluir"
     const slug = check ? "check" : "delete"
-
     const roomId = document.querySelector("#room-id").dataset.id
-
     const questionId = event.target.dataset.id
 
     const form = document.querySelector(".modal form")
     form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`)
 
-    const removeRed = modalButton.classList.remove("red")
-    const addRed = modalButton.classList.add("red")
-
-    modalTitle.innerHTML = check ? "Mark as read" : "Delete Question"
-    modalDescription.innerHTML = check ? "Are you sure to mark this question?" : "Are you sure to delete this question?"
-    modalButton.innerHTML = check ? "Yes, mark as read" : "Yes, delete"
+    modalTitle.innerHTML= `${text} esta pergunta`
+    modalDescription.innerHTML= `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+    modalButton.innerHTML= `Sim, ${text.toLowerCase()}`
     check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
-
+    
+    //abrir modal
     modal.open()
 }
